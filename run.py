c312cbc1cfb1a045cda65128aa6961bc6bb432e4
vyasj <jav_001@hotmail.com>
2025-06-06 18:41:33 -0400

    cleanup

--- run.py
+++ run.py
@@ -1,37 +1,14 @@
-import argparse
 import logging
-import os
-
-import build_db
-import dl_images
 
 
 def setup_logging(log_number):
     logging.basicConfig(
         level=logging.DEBUG,
         format='%(asctime)s - %(levelname)s - %(message)s',
-        filename=f'logging/dl_images_log_{log_number}.log',
+        filename=f'logging/run_{log_number}.log',
         filemode='a'
     )
 
 
 if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-c", "--create", help="Create database", action="store_true")
-    parser.add_argument("-p", "--populate", help="Populate database", action="store_true")
-    parser.add_argument("-d", "--dl_batch_size", help="Batch size for downloading images", default=None)
-
-    args = parser.parse_args()
-
-    
-    logfile_count = len([item for item in os.listdir("logging/") if os.path.isfile(os.path.join("logging/", item))])
-    setup_logging(logfile_count+1)
-    
-    if args.create:
-        build_db.create()
-    if args.populate:
-        build_db.populate()
-    
-    if args.dl_batch_size is not None:
-        dl_images.download(int(args.dl_batch_size))
+    pass
--- tests/test_db.py
+++ tests/test_db.py
@@ -8,22 +8,3 @@ def cursor():
     cur = cnxn.cursor()
 
     return cur
-
-
-def test_pokemon_table_exists(cursor):
-    query = "SELECT name FROM sqlite_master WHERE type='table' AND name='pokemon'"
-
-    res = cursor.execute(query)
-    tablename = res.fetchone()
-
-    assert tablename is not None
-    assert tablename[0] == 'pokemon'
-
-
-def test_last_dex_entry(cursor):
-    query = "SELECT num FROM pokemon ORDER BY num DESC"
-
-    res = cursor.execute(query)
-    last_dex_num = res.fetchone()
-
-    assert last_dex_num[0] == 1025
